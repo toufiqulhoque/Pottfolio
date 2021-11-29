8dd8c36155e7135609e9dede88737db1ba65531b
toufiqulhoque <xtoufiqxx@gmail.com>
2021-11-29 23:05:37 +0600

    third

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import './About.css'
 
 const About = () => {
     return (
-        <div className='about d-flex'>
+
+        <Container>
             <div>
-                <img src="" alt="" />
+                <h2 className='my-4'>About</h2>
+                <Row>
+                    <Col className='d-flex align-items-center justify-content-center'><img src="https://image.freepik.com/free-vector/illustration-user-avatar-icon_53876-5907.jpg" alt="" /></Col>
+                    <Col className='d-flex align-items-center justify-content-center'>
+                        <div>
+                            <h2>I'am SM Toufiqul Hoque</h2>
+                            <p>An IT professional with extensive knowledge of ReactJS, JavaScript, Tailwind, Bootstrap, CSS, HTML, and particularly mobile responsive web development, alongside the ability to write clean, DRY, and efficient code.</p>
+                        </div>
+                    </Col>
+                </Row>
+
             </div>
-            <div>
-                <h2 className='text-white'>I'am SM Toufiqul Hoque</h2>
-            </div>
-        </div>
+        </Container>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,13 @@ import About from '../About/About';
 import Banner from '../Banner/Banner';
 
 
+
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+
 
         </div>
     );
